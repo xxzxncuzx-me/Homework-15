f3c5f93687cba0e3baba32e906f5c3be76dd9905
Iryna Fedorova <i.fedorova4321@gmailcom>
2023-01-05 13:57:40 +0200

    Homework 15: modified

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
+const arr = [1, 2, 3, 4, 5, 6, 7];
 
-const removeElement = array.filter(function (el) {
-    return el !== 5
-})
+function removeElement(array, item) {
+    for (let i = 0; i < array.length; i++)
+      if (array[i] === item) {
+        array.splice(i, 1);
+      }  
+    return array;
+}
 
-console.log(removeElement)
+const result = removeElement(arr, 3)
+console.log(result);
+
+
